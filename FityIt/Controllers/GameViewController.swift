b0ea1d7c3007d0e7253f4190e439ce53e8e4eaed
Txai Wieser <txaidw@gmail.com>
2018-03-14 18:29:40 -0300

    Adding GameViewController

---
+++ FityIt/Controllers/GameViewController.swift
@@ -0,0 +1,72 @@
+//
+//  GameViewController.swift
+//  FityIt
+//
+//  Created by Txai Wieser on 13/03/18.
+//  Copyright © 2018 Txai Wieser. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import TWSpriteKitUtils
+
+class GameViewController: UIViewController {
+    var gameView: SKView { return view as! SKView }
+    
+    override func loadView() {
+        let skView = SKView()
+        skView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        skView.showsFPS = BUILD_MODE == .debug
+        skView.showsNodeCount = BUILD_MODE == .debug
+        skView.showsPhysics = BUILD_MODE == .debug
+        skView.ignoresSiblingOrder = true
+        view = skView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        TWControl.defaultTouchUpSoundFileName = "interface_sound_buttons_up.wav"
+        TWControl.defaultTouchDownSoundFileName = "interface_sound_buttons_down.wav"
+        _ = SKAction.reloadSoundEffectsSettings()
+        
+        let overlayView = UIImageView(image: UIImage(named: "bg1"))
+        overlayView.contentMode = UIViewContentMode.scaleAspectFill
+        overlayView.isUserInteractionEnabled = false
+        overlayView.alpha = 0.13
+        view.addSubview(overlayView)
+        
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+        overlayView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        overlayView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        overlayView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        #if SNAPSHOT
+            PersistenceHandler.resetUserDefaults()
+            PersistenceHandler.shouldDisplayAds = false
+            PersistenceHandler.setHighScorePoints(77)
+            let scene = InitialScene(score: Score(points: 14))
+            gameView.presentScene(scene)
+        #else
+            let scene = InitialScene(score: nil)
+            gameView.presentScene(scene)
+        #endif
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        GCHelper.sharedInstance.authenticateLocalUser()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        if let scene = gameView.scene as? InitialScene {
+            scene.gameScene = nil
+        }
+    }
+    
+    func gameEndedPresentAdAndInitialScene(_ scene: SKScene) {
+        gameView.presentScene(scene, transition: AppDefines.Transition.toInitial)
+    }
+}
