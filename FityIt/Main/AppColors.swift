bc85c40e72c7518293209782e374728920446464
Txai Wieser <txaidw@gmail.com>
2018-03-14 13:12:11 -0300

    Adding AppColors

---
+++ FityIt/Main/AppColors.swift
@@ -0,0 +1,20 @@
+//
+//  AppColors.swift
+//  FityIt
+//
+//  Created by Txai Wieser on 13/03/18.
+//  Copyright © 2018 Txai Wieser. All rights reserved.
+//
+
+
+import SpriteKit
+
+extension SKColor {
+    static let laranja: SKColor = SKColor(red:0.97, green:0.58, blue:0.31, alpha:1)
+    
+    static let rosa: SKColor = SKColor(red:0.91, green:0.31, blue:0.42, alpha:1)
+    
+    static let roxo: SKColor = SKColor(red:0.4, green:0.31, blue:0.82, alpha:1)
+    
+    static let verde: SKColor = SKColor(red:0.44, green:0.84, blue:0.25, alpha:1)
+}
