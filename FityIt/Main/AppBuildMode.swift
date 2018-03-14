667dc6f330a635b92d07e686fa374ad9f72cadea
Txai Wieser <txaidw@gmail.com>
2018-03-14 13:12:21 -0300

    Adding AppBuildMode

---
+++ FityIt/Main/AppBuildMode.swift
@@ -0,0 +1,16 @@
+//
+//  AppBuildMode.swift
+//  FityIt
+//
+//  Created by Txai Wieser on 13/03/18.
+//  Copyright © 2018 Txai Wieser. All rights reserved.
+//
+
+import Foundation
+
+enum BuildMode {
+    case debug
+    case release
+}
+
+var BUILD_MODE: BuildMode = .debug
