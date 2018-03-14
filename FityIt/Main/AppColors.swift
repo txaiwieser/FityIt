2926a8635aae5b79f17a21e7f7632733967a747f
Txai Wieser <txaidw@gmail.com>
2018-03-14 17:05:51 -0300

    Added more colors

--- FityIt/Main/AppColors.swift
+++ FityIt/Main/AppColors.swift
@@ -17,4 +17,13 @@ extension SKColor {
     static let roxo: SKColor = SKColor(red:0.4, green:0.31, blue:0.82, alpha:1)
     
     static let verde: SKColor = SKColor(red:0.44, green:0.84, blue:0.25, alpha:1)
+    
+    static let darkerLaranja: SKColor = SKColor(red:0.72, green:0.32, blue:0, alpha:1)
+    
+    static let darkerRosa: SKColor = SKColor(red:0.67, green:0.06, blue:0.17, alpha:1)
+    
+    static let darkerRoxo: SKColor = SKColor(red:0.17, green:0.01, blue:0.67, alpha:1)
+    
+    static let darkerVerde: SKColor = SKColor(red:0.22, green:0.62, blue:0, alpha:1)
+    
 }
