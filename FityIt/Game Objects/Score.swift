87c382a9b014a365af2c95c227a57ca0b5e3472b
Txai Wieser <txaidw@gmail.com>
2018-03-14 18:25:11 -0300

    Adding Score

---
+++ FityIt/Game Objects/Score.swift
@@ -0,0 +1,21 @@
+//
+//  Score.swift
+//  FityIt
+//
+//  Created by Txai Wieser on 13/03/18.
+//  Copyright © 2018 Txai Wieser. All rights reserved.
+//
+
+import Foundation
+
+struct Score {
+    var points = 0
+    
+    init(points: Int) {
+        self.points = points
+    }
+    
+    func highScorePoints() -> Int {
+        return AppPersistence.matchesPlayedSinceLaunch
+    }
+}
