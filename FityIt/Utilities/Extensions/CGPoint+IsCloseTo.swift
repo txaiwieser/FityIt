b76d04846c77cde361caa5ff0aaefd556e86a4a4
Txai Wieser <txaidw@gmail.com>
2018-03-17 12:37:16 -0300

    Adding CGPoint+IsCloseTo

---
+++ FityIt/Utilities/Extensions/CGPoint+IsCloseTo.swift
@@ -0,0 +1,24 @@
+//
+//  CGPoint+IsCloseTo.swift.swift
+//  FityIt
+//
+//  Created by Txai Wieser on 16/03/18.
+//  Copyright © 2018 Txai Wieser. All rights reserved.
+//
+
+import Foundation
+
+extension CGPoint {
+    func isClose(to point: CGPoint, with range: CGFloat) -> Bool {
+        let xMax = point.x + range
+        let xMin = point.x - range
+        let yMax = point.y + range
+        let yMin = point.y - range
+        
+        if (x >= xMin && x <= xMax) && (y >= yMin && y <= yMax) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
