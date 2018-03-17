e4a20a12d3eeeea78874aaa4324153845f825bbc
Txai Wieser <txaidw@gmail.com>
2018-03-17 14:06:41 -0300

    Refactoring AppColors

--- FityIt/Main/AppColors.swift
+++ FityIt/Main/AppColors.swift
@@ -11,25 +11,19 @@ import SpriteKit
 
 extension SKColor {
     static let mirageBlack: SKColor = SKColor(red:0.07, green:0.11, blue:0.16, alpha:1)
-    
+    static let lightBlack: SKColor = SKColor(red:0.29, green:0.35, blue:0.41, alpha:1)
     static let darkBlack: SKColor = SKColor(red:0.19, green:0.23, blue:0.27, alpha:1)
-    
     static let darkerBlack: SKColor = SKColor(red:0.03, green:0.07, blue:0.12, alpha:1)
+
     static let laranja: SKColor = SKColor(red:0.97, green:0.58, blue:0.31, alpha:1)
-    
     static let rosa: SKColor = SKColor(red:0.91, green:0.31, blue:0.42, alpha:1)
-    
     static let roxo: SKColor = SKColor(red:0.4, green:0.31, blue:0.82, alpha:1)
-    
     static let verde: SKColor = SKColor(red:0.44, green:0.84, blue:0.25, alpha:1)
     
     static let darkerLaranja: SKColor = SKColor(red:0.72, green:0.32, blue:0, alpha:1)
-    
     static let darkerRosa: SKColor = SKColor(red:0.67, green:0.06, blue:0.17, alpha:1)
-    
     static let darkerRoxo: SKColor = SKColor(red:0.17, green:0.01, blue:0.67, alpha:1)
-    
     static let darkerVerde: SKColor = SKColor(red:0.22, green:0.62, blue:0, alpha:1)
-    
+
     static let lightTintColor: SKColor = .white
 }
