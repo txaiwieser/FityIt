0ae5a1d5a18b4c878da5c52355c9378164dfed1e
Txai Wieser <txaidw@gmail.com>
2018-03-18 00:35:40 -0300

    Snapshot fixes

--- FityIt-Screenshots/FityIt_Screenshots.swift
+++ FityIt-Screenshots/FityIt_Screenshots.swift
@@ -21,25 +21,25 @@ class FityIt_Screenshots: XCTestCase {
         let element1 = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element
         
         sleep(2)
-        snapshot("1-InitialScreen")
+        snapshot("1-GameScreen")
         element1.tap()
         sleep(1)
         
         element1.tap()
         sleep(2)
-        snapshot("2-InitialScreen")
+        snapshot("2-GameScreen")
         
         element1.tap()
         sleep(1)
-        snapshot("3-InitialScreen")
+        snapshot("3-GameScreen")
         
         element1.tap()
         sleep(2)
-        snapshot("4-InitialScreen")
+        snapshot("4-GameScreen")
         
         element1.tap()
         sleep(1)
-        snapshot("5-InitialScreen")
+        snapshot("5-GameScreen")
         
     }
 }
